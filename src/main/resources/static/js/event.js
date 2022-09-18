1ba66556bff7e6b7f47e4923f91bb6e91b2c7238
wndudrla1011 <wndudrla1011@naver.com>
2022-09-18 21:53:04 +0900

    added hover listener

--- src/main/resources/static/js/event.js
+++ src/main/resources/static/js/event.js
@@ -1,15 +1,25 @@
 var main = {
     //각 버튼에 대한 이벤트 처리
-    init : function (){
+    init : function () {
         var _this = this;
-        $('#menu-bar').on('click', function () {
-            _this.appear();
-        });
+        $('#menu-bar').hover(
+            function () {
+                _this.appear();
+            },
+            function () {
+                _this.disappear();
+            }
+        );
     },
 
     appear : function () {
             $('#sidebars').removeClass('visually-hidden');
             $('#menu-bar').addClass('visually-hidden');
+        },
+
+    disappear : function () {
+            $('#sidebars').addClass('visually-hidden');
+            $('#menu-bar').removeClass('visually-hidden');
         }
 
 };
