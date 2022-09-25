f4762ef2c3857d03a9893c2db19b152f1e82bb58
wndudrla1011 <wndudrla1011@naver.com>
2022-09-25 16:50:09 +0900

    added GetMapping to postInfo.html

--- src/main/java/com/rootable/libraryservice2022/controller/PostsController.java
+++ src/main/java/com/rootable/libraryservice2022/controller/PostsController.java
@@ -15,10 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
@@ -123,6 +120,18 @@ public class PostsController {
         }
 
         return "redirect:/posts";
+
+    }
+
+    @GetMapping("/posts/{postId}")
+    public String post(@PathVariable Long postId, Model model) {
+
+        log.info("게시글 정보");
+
+        PostDto post = postsService.getPost(postId);
+        model.addAttribute("post", post);
+
+        return "/posts/postInfo";
 
     }
 
