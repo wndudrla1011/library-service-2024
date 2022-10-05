f1b069735f0ffadd0b4d9c54013346da60a5553d
wndudrla1011 <wndudrla1011@naver.com>
2022-10-05 20:42:55 +0900

    added btn move to admin page if member is ADMIN or STAFF

--- src/main/java/com/rootable/libraryservice2022/controller/HomeController.java
+++ src/main/java/com/rootable/libraryservice2022/controller/HomeController.java
@@ -1,6 +1,6 @@
 package com.rootable.libraryservice2022.controller;
 
-import com.rootable.libraryservice2022.domain.Member;
+import com.rootable.libraryservice2022.domain.Role;
 import com.rootable.libraryservice2022.web.argumentresolver.Login;
 import com.rootable.libraryservice2022.web.dto.SessionMember;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Slf4j
 public class HomeController {
 
+    public static String ADMIN_KEY = "admin";
+
     @GetMapping("/")
     public String homeLogin(@Login SessionMember loginMember, Model model) {
 
@@ -20,6 +22,10 @@ public class HomeController {
         //세션에 회원 데이터가 없으면 intro 화면으로
         if (loginMember == null) {
             return "index";
+        }
+
+        if (loginMember.getRole() == Role.ADMIN || loginMember.getRole() == Role.STAFF) {
+            model.addAttribute("admin", ADMIN_KEY);
         }
 
         //회원 데이터가 있으면 로그인 화면으로
