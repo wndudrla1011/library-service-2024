ecc23490bcb3b5ca05fbafbe12f6d7b91a4cb26c
wndudrla1011 <wndudrla1011@naver.com>
2022-10-11 21:54:27 +0900

    test CommentServiceTest.myComments

--- src/test/java/com/rootable/libraryservice2022/service/CommentServiceTest.java
+++ src/test/java/com/rootable/libraryservice2022/service/CommentServiceTest.java
@@ -7,7 +7,9 @@ import com.rootable.libraryservice2022.repository.PostsRepository;
 import com.rootable.libraryservice2022.web.dto.BookSaveDto;
 import com.rootable.libraryservice2022.web.dto.CommentRequestDto;
 import com.rootable.libraryservice2022.web.dto.MemberDto;
+import com.rootable.libraryservice2022.web.dto.PostDto;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,6 +17,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.Assert.*;
@@ -25,6 +29,7 @@ import static org.junit.Assert.*;
 public class CommentServiceTest {
 
     @Autowired CommentService commentService;
+    @Autowired PostsService postsService;
     @Autowired PostsRepository postsRepository;
     @Autowired MemberRepository memberRepository;
     @Autowired BookRepository bookRepository;
@@ -187,6 +192,85 @@ public class CommentServiceTest {
 
         //then
         assertThat(entityManager.contains(comment)).isFalse();
+
+    }
+
+    @Test
+    @DisplayName("나의 댓글 조회")
+    public void myComments() {
+
+        //given
+        MemberDto memberDto1 = MemberDto.builder()
+                .name("kim")
+                .loginId("test2")
+                .password("1111!!qq")
+                .email("test2@gmail.com")
+                .role(Role.USER)
+                .build();
+
+        MemberDto memberDto2 = MemberDto.builder()
+                .name("Lee")
+                .loginId("staff11")
+                .password("1111!!rr")
+                .email("staff11@gmail.com")
+                .role(Role.STAFF)
+                .build();
+
+        Member myOne = createMember(memberDto1);
+        Member another = createMember(memberDto2);
+
+        Member savedMyOne = memberRepository.save(myOne);
+        Member savedAnother = memberRepository.save(another);
+
+        BookSaveDto bookDto = BookSaveDto.builder()
+                .title("자바 기초")
+                .writer("TonyJoo")
+                .price(10000)
+                .stock(3)
+                .status(Status.PERMISSION)
+                .build();
+
+        Book book = createBook(bookDto);
+        bookRepository.save(book);
+
+        PostDto myPostDto = PostDto.builder()
+                .title("게시글")
+                .content("hello")
+                .member(myOne)
+                .book(book)
+                .build();
+
+        Long savedPostId = postsService.savePost(myPostDto);
+        Posts posts = postsService.findById(savedPostId);
+
+        CommentRequestDto myCommentDto = CommentRequestDto.builder()
+                .comment("나의 댓글")
+                .member(another)
+                .posts(posts)
+                .build();
+
+        CommentRequestDto anotherCommentDto = CommentRequestDto.builder()
+                .comment("직원 댓글")
+                .member(another)
+                .posts(posts)
+                .build();
+
+        commentService.commentSave(savedMyOne.getId(), posts.getId(), myCommentDto);
+        commentService.commentSave(savedAnother.getId(), posts.getId(), anotherCommentDto);
+
+        //when
+        List<Comment> myComments = commentService.findMyComments(myOne.getId());
+        Comment findMyComment = myComments.stream()
+                .filter(my -> my.getComment().equals("나의 댓글"))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("해당 댓글이 존재하지 않습니다. comment=" + myCommentDto.getComment()));
+
+        //then
+        assertThat(myComments.contains(findMyComment)).isTrue();
+        assertThat(myComments.stream()
+                .filter(c -> c.getComment().equals("직원 댓글"))
+                .findFirst().isEmpty()).isTrue();
+
 
     }
 
