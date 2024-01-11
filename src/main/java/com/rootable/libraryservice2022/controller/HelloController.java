1b6cbb8634b73d97aa8a4725c74f2a8b10cba4af
wndudrla1011 <wndudrla1011@naver.com>
2024-01-11 16:08:24 +0900

    testing controller that received data from App.js

---
+++ src/main/java/com/rootable/libraryservice2022/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.rootable.libraryservice2022.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/hello")
+    public String test() {
+        return "Hello, world!";
+    }
+
+}
