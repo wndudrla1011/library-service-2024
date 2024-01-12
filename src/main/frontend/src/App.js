3f2d9cc3c48771d5fb0f446d0c3e5bdcde133abb
wndudrla1011 <wndudrla1011@naver.com>
2024-01-12 20:03:26 +0900

    updated App.js(index.html)

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -3,23 +3,33 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
-  const [hello, setHello] = useState([]);
-
-  useEffect((data) => {
-    axios
-      .get("/api/hello")
-      .then((response) => setHello(response.data))
-      .catch((error) => console.log(error));
-  }, []);
-
   return (
     <div>
-      <p>{hello.id}</p>
-      <p>{hello.name}</p>
-      <p>{hello.loginId}</p>
-      <p>{hello.passwod}</p>
-      <p>{hello.email}</p>
-      <p>{hello.role}</p>
+      <header>
+        <div>
+          <h3>Rootable Library</h3>
+        </div>
+      </header>
+
+      <main>
+        <h1>
+          편리한 도서 신청 서비스를 <br>지금</br> 이용해 보세요!
+        </h1>
+        <p>
+          서비스 이용을 위해
+          <br>회원가입 또는 로그인이 필요합니다.</br>
+        </p>
+        <p>
+          <button>회원가입</button>
+          <button>로그인</button>
+        </p>
+      </main>
+
+      <footer>
+        <p>
+          Developed by <a>Joo Yound, Kim</a>, <p>Ver.2024V2</p>
+        </p>
+      </footer>
     </div>
   );
 }
