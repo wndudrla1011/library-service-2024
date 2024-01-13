6c8664e81d32eb7b40e2ae024b12d1c5ca20c8e6
wndudrla1011 <wndudrla1011@naver.com>
2024-01-13 19:35:00 +0900

    added .prettierrc and bootstrap

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,15 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React, { useState, useEffect } from "react";
-import Home from "./Home";
 
 function App() {
-  return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
-  );
+  return <div></div>;
 }
 
 export default App;
