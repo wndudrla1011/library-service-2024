636bfb908338d098f09e2e1838485d1dda9174f5
wndudrla1011 <wndudrla1011@naver.com>
2024-01-15 14:48:45 +0900

    imported PostItem to Home

--- src/main/frontend/src/pages/Home.js
+++ src/main/frontend/src/pages/Home.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import PostItem from '../components/PostItem';
 
 function Home() {
   return (
     <div>
-      <h1>Main Page</h1>
+      <PostItem />
     </div>
   );
 }
