355fd85037164615221bf3113faa249a299932d2
wndudrla1011 <wndudrla1011@naver.com>
2024-01-16 21:23:16 +0900

    testing fetch to 'localhost:8080/posts' (failed for CORS)

--- src/main/frontend/src/pages/Home.js
+++ src/main/frontend/src/pages/Home.js
@@ -4,6 +4,15 @@ import { Link } from 'react-router-dom';
 import PostItem from '../components/PostItem';
 
 function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/posts')
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(1, res);
+      });
+  }, []);
   return (
     <div>
       <PostItem />
