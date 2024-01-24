540caf475e1e15c420a2f74da6431a7572337db2
wndudrla1011 <wndudrla1011@naver.com>
2024-01-24 18:46:40 +0900

    defined the API ralated to member (login, detail, join, update, delete)

---
+++ src/main/frontend/src/apis/auth.js
@@ -0,0 +1,18 @@
+import api from './api';
+
+//로그인
+export const login = (loginId, password) =>
+  api.post(`/login?loginId=${loginId}&password=${password}`);
+
+//사용자 정보
+export const info = () => api.get(`/admin/members/${memberId}`);
+
+//회원가입
+export const join = (data) => api.post(`/members/add`, data);
+
+//회원 정보 수정
+export const update = (data) =>
+  api.put(`/admin/members/${memberId}/edit`, data);
+
+//회원 탈퇴
+export const remove = (memberId) => api.delete(`/admin/members`);
