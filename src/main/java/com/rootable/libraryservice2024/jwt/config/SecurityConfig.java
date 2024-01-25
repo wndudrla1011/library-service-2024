9c0969a88b29f295106b1832cdc95bb1b3a0bb6b
wndudrla1011 <wndudrla1011@naver.com>
2024-01-25 16:43:48 +0900

    created SecurityConfig for Spring Security

---
+++ src/main/java/com/rootable/libraryservice2024/jwt/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.rootable.libraryservice2024.jwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        //폼 기반 로그인 비활성화
+        httpSecurity.formLogin().disable();
+
+        //HTTP 기본 인증 비활성화
+
+        //CSRF 공격 방어 기능 비활성화
+
+        /*
+        * 세션 관리 정책 설정
+        * 세션 인증 사용x => JWT를 사용하여 인증
+        * */
+
+
+    }
+
+}
