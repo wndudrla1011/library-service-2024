1f03410e84ca1fd1b56a13900f83a8c62681488c
wndudrla1011 <wndudrla1011@naver.com>
2024-01-25 17:03:50 +0900

    disabled to configurations of Spring Security

--- src/main/java/com/rootable/libraryservice2024/jwt/config/SecurityConfig.java
+++ src/main/java/com/rootable/libraryservice2024/jwt/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,14 +18,18 @@ public class SecurityConfig {
         httpSecurity.formLogin().disable();
 
         //HTTP 기본 인증 비활성화
+        httpSecurity.httpBasic().disable();
 
         //CSRF 공격 방어 기능 비활성화
+        httpSecurity.csrf().disable();
 
         /*
         * 세션 관리 정책 설정
         * 세션 인증 사용x => JWT를 사용하여 인증
         * */
+        httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
+        return httpSecurity.build();
 
     }
 
