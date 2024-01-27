c9d0d9e2548edfe99c9af746a27a92724363b04a
wndudrla1011 <wndudrla1011@naver.com>
2024-01-27 09:37:00 +0900

    added PasswordEncoder

--- src/main/java/com/rootable/libraryservice2024/jwt/config/SecurityConfig.java
+++ src/main/java/com/rootable/libraryservice2024/jwt/config/SecurityConfig.java
@@ -1,35 +1,33 @@
 package com.rootable.libraryservice2024.jwt.config;
 
+import com.rootable.libraryservice2024.jwt.provider.TokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final TokenProvider tokenProvider;
+
+    //PasswordEncoder
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
-        //폼 기반 로그인 비활성화
-        httpSecurity.formLogin().disable();
 
-        //HTTP 기본 인증 비활성화
-        httpSecurity.httpBasic().disable();
-
-        //CSRF 공격 방어 기능 비활성화
-        httpSecurity.csrf().disable();
-
-        /*
-        * 세션 관리 정책 설정
-        * 세션 인증 사용x => JWT를 사용하여 인증
-        * */
-        httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-        return httpSecurity.build();
 
     }
 
