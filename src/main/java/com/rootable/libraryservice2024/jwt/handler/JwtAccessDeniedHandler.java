76497d4c10fe74d26f35fdf9d514e5186da7ab05
wndudrla1011 <wndudrla1011@naver.com>
2024-01-31 17:17:07 +0900

    created JwtAccessDeniedHandler to handle forbidden access

---
+++ src/main/java/com/rootable/libraryservice2024/jwt/handler/JwtAccessDeniedHandler.java
@@ -0,0 +1,23 @@
+package com.rootable.libraryservice2024.jwt.handler;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+        //필요한 권한이 없이 접근하려 할 때 403 error
+        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+
+    }
+
+}
