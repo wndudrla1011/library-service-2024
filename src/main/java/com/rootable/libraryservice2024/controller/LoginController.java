a0a2ca2291d833712dbf8a4862fc05a10bef2256
wndudrla1011 <wndudrla1011@naver.com>
2024-02-01 10:05:50 +0900

    edited loginId -> username in LoginDto and removed areas to test token in LoginController

--- src/main/java/com/rootable/libraryservice2024/controller/LoginController.java
+++ src/main/java/com/rootable/libraryservice2024/controller/LoginController.java
@@ -1,17 +1,7 @@
 package com.rootable.libraryservice2024.controller;
 
-import com.rootable.libraryservice2024.domain.Member;
-import com.rootable.libraryservice2024.domain.Role;
-import com.rootable.libraryservice2024.jwt.constants.SecurityConstants;
-import com.rootable.libraryservice2024.jwt.prop.JwtProp;
 import com.rootable.libraryservice2024.service.LoginService;
 import com.rootable.libraryservice2024.web.dto.LoginDto;
-import com.rootable.libraryservice2024.web.dto.SessionMember;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -20,9 +10,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 @Slf4j
 @RestController
@@ -31,7 +18,6 @@ public class LoginController {
 
     private final LoginService loginService;
     private final HttpSession httpSession;
-    private final JwtProp jwtProp;
 
     @GetMapping("/login")
     public String loginForm(@ModelAttribute("loginForm") LoginDto dto) {
@@ -44,61 +30,13 @@ public class LoginController {
 
         log.info("로그인 검증");
 
-        String loginId = loginDto.getLoginId();
+        String username = loginDto.getUsername();
         String password = loginDto.getPassword();
 
-        log.info("loginId : " + loginId);
+        log.info("username : " + username);
         log.info("password : " + password);
 
-        List<String> roles = new ArrayList();
-        roles.add(Role.ADMIN.getKey());
-        roles.add(Role.STAFF.getKey());
-        roles.add(Role.GUEST.getKey());
-        roles.add(Role.USER.getKey());
-
-        //secret key -> bytes
-        byte[] signingKey = jwtProp.getSecretKey().getBytes();
-
-        //토큰 생성
-        String jwt = Jwts.builder()
-                .signWith(SignatureAlgorithm.HS512, Keys.hmacShaKeyFor(signingKey)) //hash algorithm, secret key for signature
-                .setHeaderParam("typ", SecurityConstants.TOKEN_TYPE) //header configuration {typ : JWT}
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 3)) //exp: 3 min
-                .claim("uid", loginId) //payload - uid
-                .claim("role", roles) //payload - role
-                .compact();
-
-        log.info("jwt : " + jwt);
-
-        return new ResponseEntity<>(jwt, HttpStatus.OK);
-
-    }
-
-    //토큰 해석
-    @GetMapping("/member/info")
-    public ResponseEntity<?> validateToken(@RequestHeader(name = "Authorization") String header) {
-
-        log.info("토큰 해석");
-        log.info("Authorization : " + header);
-
-        //Prefix 제거
-        String jwt = header.replace(SecurityConstants.TOKEN_PREFIX, "");
-
-        byte[] signingKey = jwtProp.getSecretKey().getBytes();
-
-        //Decode Token
-        Jws<Claims> parsedToken = Jwts.parser().setSigningKey(signingKey).parseClaimsJws(jwt);
-
-        log.info("parsedToken : " + parsedToken);
-
-        //Get Payload
-        String loginId = parsedToken.getBody().get("uid").toString();
-        log.info("loginId : " + loginId);
-
-        Object roles = parsedToken.getBody().get("role");
-        log.info("role : " + roles);
-
-        return new ResponseEntity<>(parsedToken.toString(), HttpStatus.OK);
+        return new ResponseEntity<>("login", HttpStatus.OK);
 
     }
 
--- src/main/java/com/rootable/libraryservice2024/web/dto/LoginDto.java
+++ src/main/java/com/rootable/libraryservice2024/web/dto/LoginDto.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.NotBlank;
 public class LoginDto {
 
     @NotBlank(message = "아이디를 입력해주세요")
-    private String loginId;
+    private String username;
 
     @NotBlank(message = "비밀번호를 입력해주세요")
     private String password;
