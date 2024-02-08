035f4efe655ec29df37ac036e9e5a13c6a45afd0
wndudrla1011 <wndudrla1011@naver.com>
2024-02-08 19:40:44 +0900

    declared constants for TokenProvider and bring secretKey

---
+++ src/main/java/com/rootable/libraryservice2024/jwt/provider/TokenProvider.java
@@ -0,0 +1,27 @@
+package com.rootable.libraryservice2024.jwt.provider;
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Slf4j
+@Component
+public class TokenProvider {
+
+    private static final String AUTHORITIES_KEY = "auth";
+    private static final String BEARER_TYPE = "Bearer";
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30; //30min
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7; //7 days
+
+    private final Key key;
+
+    public TokenProvider(@Value("${jwt.secret}") String secretKey) {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+}
