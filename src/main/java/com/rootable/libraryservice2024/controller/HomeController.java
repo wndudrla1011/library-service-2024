1ee3ed20f751f480f468e39ba87e2331ca6fdda5
wndudrla1011 <wndudrla1011@naver.com>
2024-02-08 19:25:47 +0900

    edited HomeController

--- src/main/java/com/rootable/libraryservice2024/controller/HomeController.java
+++ src/main/java/com/rootable/libraryservice2024/controller/HomeController.java
@@ -1,9 +1,6 @@
 package com.rootable.libraryservice2024.controller;
 
-import com.rootable.libraryservice2024.web.argumentresolver.Login;
-import com.rootable.libraryservice2024.web.dto.SessionMember;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +11,7 @@ public class HomeController {
     public static String ADMIN_KEY = "admin";
 
     @GetMapping("/")
-    public String homeLogin(@Login SessionMember loginMember, Model model) {
+    public String homeLogin() {
 
         log.info("홈 화면 이동");
 
