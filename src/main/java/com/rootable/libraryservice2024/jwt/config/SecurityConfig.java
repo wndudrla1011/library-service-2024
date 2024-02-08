2838ba65b8172a72d1d6c60a553614d26c674e1b
wndudrla1011 <wndudrla1011@naver.com>
2024-02-08 15:06:47 +0900

    created SecurityConfig

--- src/main/java/com/rootable/libraryservice2024/jwt/config/SecurityConfig.java
+++ src/main/java/com/rootable/libraryservice2024/jwt/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.rootable.libraryservice2024.jwt.config;
 
-import com.rootable.libraryservice2024.jwt.provider.TokenProvider;
+import com.rootable.libraryservice2024.jwt.handler.JwtAccessDeniedHandler;
+import com.rootable.libraryservice2024.jwt.handler.JwtAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,13 +11,15 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
     //PasswordEncoder
     @Bean
@@ -27,7 +30,37 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
+        httpSecurity
+                //token 방식 => off the csrf/form login/httpBasic
+                .csrf().disable()
+                .formLogin().disable()
+                .httpBasic().disable()
 
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                //enable h2-console
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                //세션을 사용하지 않기 때문에 STATELESS로 설정
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+
+                //HttpServletRequest를 사용하는 요청들에 대한 접근제한 설정
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers(new AntPathRequestMatcher("/")).permitAll() //홈
+                        .requestMatchers(new AntPathRequestMatcher("/login")).permitAll() //로그인 api
+                        .requestMatchers(new AntPathRequestMatcher("/members/add")).permitAll() //회원가입 api
+                        .requestMatchers(new AntPathRequestMatcher("/admin")).hasRole("ADMIN") //관리자 페이지에 요구되는 권한 설정
+                        .anyRequest().authenticated()); //그 외 인증 없이 접근x
+
+        return httpSecurity.build();
 
     }
 
