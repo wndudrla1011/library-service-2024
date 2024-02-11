061af171ecea7176fb9eb8a4970a36a8f66671e6
wndudrla1011 <wndudrla1011@naver.com>
2024-02-11 17:37:28 +0900

    remove existByUsername and added findOneWithAuthoritiesByUsername

--- src/main/java/com/rootable/libraryservice2024/repository/MemberRepository.java
+++ src/main/java/com/rootable/libraryservice2024/repository/MemberRepository.java
@@ -1,15 +1,18 @@
 package com.rootable.libraryservice2024.repository;
 
 import com.rootable.libraryservice2024.domain.Member;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    Boolean existByUsername(String username); //회원 존재 여부
+    @EntityGraph(attributePaths = "authorities")
+    Optional<Member> findOneWithAuthoritiesByUsername(String username);
 
     @Query("select m from Member m order by m.id desc")
     List<Member> findMembers();
