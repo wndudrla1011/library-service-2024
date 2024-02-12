a987b6189a7ac8d0d3ca654de412b1d0edef8003
wndudrla1011 <wndudrla1011@naver.com>
2024-02-12 14:44:12 +0900

    edited data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,2 @@
-insert into member (member_id, username, password, nickname, activated, email)
-values (1, 'admin', '$2a$08$loDOSxczwXlrG90e31NkNOx9JquYMrwDixVdu2kLvVCbPDtt2SbZq', 'admin', 1, 'admin@gmail.com');
-
-insert into authority (authority_name) values ('ROLE_USER');
-insert into authority (authority_name) values ('ROLE_ADMIN');
-
-insert into user_authority (member_id, authority_name) values (1, 'ROLE_USER');
-insert into user_authority (member_id, authority_name) values (1, 'ROLE_ADMIN');
+insert into member (member_id, email, password, nickname, authority)
+values (1, 'admin@gmail.com', '$2a$08$loDOSxczwXlrG90e31NkNOx9JquYMrwDixVdu2kLvVCbPDtt2SbZq', 'admin', 'ROLE_ADMIN');
