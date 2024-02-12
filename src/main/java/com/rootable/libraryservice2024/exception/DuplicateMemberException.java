9c8c4871facb544b2b9d78b723ee1c0aaae485a8
wndudrla1011 <wndudrla1011@naver.com>
2024-02-12 13:38:38 +0900

    created DuplicateMemberException

---
+++ src/main/java/com/rootable/libraryservice2024/exception/DuplicateMemberException.java
@@ -0,0 +1,21 @@
+package com.rootable.libraryservice2024.exception;
+
+public class DuplicateMemberException extends RuntimeException {
+
+    public DuplicateMemberException() {
+        super();
+    }
+
+    public DuplicateMemberException(String message) {
+        super(message);
+    }
+
+    public DuplicateMemberException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DuplicateMemberException(Throwable cause) {
+        super(cause);
+    }
+
+}
