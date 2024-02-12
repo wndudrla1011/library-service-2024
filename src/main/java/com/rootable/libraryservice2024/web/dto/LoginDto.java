a412ef8676bd5738eb01a539b539e3f72792a37b
wndudrla1011 <wndudrla1011@naver.com>
2024-02-12 14:43:58 +0900

    edited LoginDto (username -> email)

--- src/main/java/com/rootable/libraryservice2024/web/dto/LoginDto.java
+++ src/main/java/com/rootable/libraryservice2024/web/dto/LoginDto.java
@@ -9,8 +9,8 @@ import javax.validation.constraints.NotBlank;
 @NoArgsConstructor
 public class LoginDto {
 
-    @NotBlank(message = "아이디를 입력해주세요")
-    private String username;
+    @NotBlank(message = "이메일을 입력해주세요")
+    private String email;
 
     @NotBlank(message = "비밀번호를 입력해주세요")
     private String password;
