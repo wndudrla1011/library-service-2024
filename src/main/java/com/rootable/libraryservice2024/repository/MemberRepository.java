3cad630adaa2e74cd636f74f7668fb1d6cf93cf1
wndudrla1011 <wndudrla1011@naver.com>
2024-02-12 14:04:56 +0900

    removed not used imports

--- src/main/java/com/rootable/libraryservice2024/repository/MemberRepository.java
+++ src/main/java/com/rootable/libraryservice2024/repository/MemberRepository.java
@@ -3,7 +3,6 @@ package com.rootable.libraryservice2024.repository;
 import com.rootable.libraryservice2024.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
