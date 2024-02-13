6c6cadf394347ea7b8b1b3ca76714e0c3b871145
wndudrla1011 <wndudrla1011@naver.com>
2024-02-13 18:44:15 +0900

    removed all and added findOneWithAuthoritiesByEmail

--- src/main/java/com/rootable/libraryservice2024/repository/MemberRepository.java
+++ src/main/java/com/rootable/libraryservice2024/repository/MemberRepository.java
@@ -1,14 +1,14 @@
 package com.rootable.libraryservice2024.repository;
 
 import com.rootable.libraryservice2024.domain.Member;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    Optional<Member> findByEmail(String email);
-
-    boolean existsByEmail(String email);
+    @EntityGraph(attributePaths = "authorities")
+    Optional<Member> findOneWithAuthoritiesByEmail(String email);
 
 }
