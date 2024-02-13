5d9513bdc6ca5127129ad181c41708fdb5296c6b
wndudrla1011 <wndudrla1011@naver.com>
2024-02-13 22:05:57 +0900

    removed not used imports

--- src/main/java/com/rootable/libraryservice2024/controller/AuthController.java
+++ src/main/java/com/rootable/libraryservice2024/controller/AuthController.java
@@ -1,20 +1,12 @@
 package com.rootable.libraryservice2024.controller;
 
-import com.rootable.libraryservice2024.jwt.JwtFilter;
-import com.rootable.libraryservice2024.jwt.TokenProvider;
 import com.rootable.libraryservice2024.service.AuthService;
 import com.rootable.libraryservice2024.web.dto.LoginDto;
 import com.rootable.libraryservice2024.web.dto.MemberDto;
 import com.rootable.libraryservice2024.web.dto.TokenDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
